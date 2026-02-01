cf12556b405f6931ec33e0461836e59fa48ae8ba
Alpine Chamois <79454775+alpine-chamois@users.noreply.github.com>
2026-02-01 17:59:55 +0000

    Initial commit

---
+++ src/visdoom/constants.py
@@ -0,0 +1,10 @@
+# ViZDoom
+ENV_ID: str = "vis-doom-v0"
+SCREEN_KEY = "screen"
+
+# Model folder
+MODEL_FOLDER: str = "models"
+
+# Gymnasium render modes
+RGB: str = "rgb_array"
+HUMAN: str = "human"
